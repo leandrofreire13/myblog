fe7e07f3438967a7900dffc31390bb07e2a9e42e
leandrofreire13 <rafaelleandrofreire@gmail.com>
2023-01-31 20:15:52 -0300

    Imagem no post

--- blog/models.py
+++ blog/models.py
@@ -14,6 +14,7 @@ class Post(models.Model):
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
+    imagem = models.ImageField(upload_to='media/', default='')
 
     class Meta:
         ordering = ['-created_on']
